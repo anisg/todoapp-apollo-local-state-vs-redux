86799a6d9c358e54215f468b09fa690eb6a87f14
aniskv <agadhgad@gmail.com>
2020-01-27 14:34:57 +0100

    Updates

---
+++ precache-manifest.b013c199eb2e47025fb51ad0ea2702fc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7357587ed3b8fe22fb41e6929e8c4de9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9711902ea2e063023c71",
+    "url": "/static/css/main.3d54d8d9.chunk.css"
+  },
+  {
+    "revision": "e246448bad52e55a0d12",
+    "url": "/static/js/2.d7679731.chunk.js"
+  },
+  {
+    "revision": "1af6562b6d659632968c2e5e85a5b301",
+    "url": "/static/js/2.d7679731.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9711902ea2e063023c71",
+    "url": "/static/js/main.311080cb.chunk.js"
+  },
+  {
+    "revision": "b61731232424dc4083c6",
+    "url": "/static/js/runtime-main.b244248e.js"
+  }
+]);
